f43c4b10968d7adda99a2709e084ade5a451938c
NexusRule <85776211+NexusRule@users.noreply.github.com>
2022-06-28 19:11:58 -0500

    Update digitalInputs.ino

--- src/digitalInputs.ino
+++ src/digitalInputs.ino
@@ -1,16 +1,18 @@
+// i have no clue how this worked, i used pins D5 and D7-
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 bool value;
 
 void setup() {
+  pinMode(D5, OUTPUT);
   pinMode(D7, INPUT);
 }
 
 void loop() {
   value = digitalRead(D7);
   if (value == true) {
-    digitalWrite(D6, HIGH);
+    digitalWrite(D5, HIGH);
   } else {
-    digitalWrite(D6, LOW);
+    digitalWrite(D5, LOW);
   }
 }
